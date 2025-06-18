818173c11aebd011c0c69222f57586a578d97a76
JosephNgabo <joseph.ntwali@adfinance.co>
2025-06-18 15:06:45 +0200

    fixing env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,7 @@
 export const environment = {
   production: true,
   defaultauth: 'fakebackend',
+  apiUrl: 'https://your-api-url.com/api',
   firebaseConfig: {
     apiKey: '',
     authDomain: '',
